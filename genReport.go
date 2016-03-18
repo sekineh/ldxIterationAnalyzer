099cf7f743141a16540ade2d53166e45dce5ad77
SEKINE, Hideki <sekineh@me.com>
2016-03-18 19:29:27 +0900

    Run go fmt + add err checking.

--- genReport.go
+++ genReport.go
@@ -1,41 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "io/ioutil"
-    // "time"
-    "bytes"
-    "path/filepath"
-    // "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	// "time"
+	"bytes"
+	"path/filepath"
+	// "strings"
 )
 
-func reportFileName(filename string) (string) {
-    _, fname := filepath.Split(filename)
-    return fmt.Sprintf("report_%s.html", fname)
+func reportFileName(filename string) string {
+	_, fname := filepath.Split(filename)
+	return fmt.Sprintf("report_%s.html", fname)
 }
 
 func main() {
-    templatefile := "template.html"
-    csvpath := os.Args[1]
-    _, csvfile := filepath.Split(csvpath)
+	templatefile := "template.html"
+	csvpath := os.Args[1]
+	_, csvfile := filepath.Split(csvpath)
 
-    template, _ := ioutil.ReadFile(templatefile)
-    csv, _ := ioutil.ReadFile(csvpath)
-    version, _ := ioutil.ReadFile("version")
+	template, err := ioutil.ReadFile(templatefile)
+	if err != nil {
+		panic(err)
+	}
+	csv, err := ioutil.ReadFile(csvpath)
+	if err != nil {
+		panic(err)
+	}
+	version, err := ioutil.ReadFile("version")
+	if err != nil {
+		panic(err)
+	}
 
-    // fmt.Print(string(template)[:10])
-    // fmt.Print(string(csv[:10]))
+	// fmt.Print(string(template)[:10])
+	// fmt.Print(string(csv[:10]))
 
-    report := template
-    report = bytes.Replace(report, []byte("{csv_here}"), csv, 1)
-    report = bytes.Replace(report, []byte("{filename_here}"), []byte(csvfile), 1)
-    report = bytes.Replace(report, []byte("{version_here}"), version, 1)
+	report := template
+	report = bytes.Replace(report, []byte("{csv_here}"), csv, 1)
+	report = bytes.Replace(report, []byte("{filename_here}"), []byte(csvfile), 1)
+	report = bytes.Replace(report, []byte("{version_here}"), version, 1)
 
-    // fmt.Print(string(report))
-    // fmt.Print(reportFileName(csvfile))
+	// fmt.Print(string(report))
+	// fmt.Print(reportFileName(csvfile))
 
-    ioutil.WriteFile(reportFileName(csvfile), report, 0644)
+	ioutil.WriteFile(reportFileName(csvfile), report, 0644)
 
-    // time.Sleep(10000)
+	// time.Sleep(10000)
 }
